5840694c8c1d3ce5503d3d7c2a68d605a778f8c4
Dave Blencowe <dblencowe@gmail.com>
2016-11-18 14:34:03 +0000

    Use illuminate database connections

--- src/Dblencowe/ScrubberDummy/Scrubber.php
+++ src/Dblencowe/ScrubberDummy/Scrubber.php
@@ -1,12 +1,14 @@
 <?php
 
 namespace Dblencowe\ScrubberDummy;
+use Illuminate\Database\MySqlConnection;
 
 class Scrubber
 {
 
-    public function scrub($databaseHandler, $databaseName)
+    public function scrub(MySqlConnection $databaseHandler, $databaseName)
     {
-        echo 'Hello world!';
+        $databaseHandler->setDatabaseName($databaseName);
+        return $databaseHandler->select('SELECT * FROM poll');
     }
 }
